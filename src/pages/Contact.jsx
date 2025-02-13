81287255348d37dae8370ac2e0abab7643b177ef
enamul Hasan Himel <mhasan173013@bscse.uiu.ac.bd>
2025-02-13 17:27:31 +0600

    header and footer added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,134 @@
-import React from 'react'
+import { Button, Form, Input } from "@heroui/react";
+import React, { useState } from "react";
+import CommonWrapper from "../components/CommonWrapper";
+import arrow from "../assets/images/arrow.png";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [massage, setMassage] = useState("");
+  const [submitted, setSubmitted] = useState(null);
 
-export default Contact
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const data = Object.fromEntries(new FormData(e.currentTarget));
+
+    setSubmitted(data);
+    console.log(data);
+  };
+
+  return (
+    <CommonWrapper>
+      <div className="pt-[140px] ">
+        <h3 className="text-[48px] font-semibold leading-[63px] text-center">
+          Send Your Massage
+        </h3>
+        <div className="lg:pl-[250px] xl:pl-[250px] pt-[64px] items-center">
+          <Form
+            className="w-full max-w-xs"
+            validationBehavior="native"
+            onSubmit={onSubmit}
+          >
+            <div className="lg:flex xl:flex gap-[20px]">
+              <div>
+                <Input
+                  isRequired
+                  errorMessage="Please enter your first Name"
+                  label="First Name"
+                  labelPlacement="outside"
+                  name="first name"
+                  placeholder="Enter your first name"
+                  type="text"
+                  value={firstName}
+                  onValueChange={setFirstName}
+                  style={{ width: "490px", height: "64px" }}
+                />
+              </div>
+              <div>
+                <Input
+                  isRequired
+                  errorMessage="Please enter your last name"
+                  label="Last Name"
+                  labelPlacement="outside"
+                  name="last name"
+                  placeholder="Please enter your last name"
+                  type="text"
+                  value={lastName}
+                  onValueChange={setLastName}
+                  style={{ width: "490px", height: "64px" }}
+                />
+              </div>
+            </div>
+            <div className="lg:flex xl:flex gap-[20px]">
+              <div>
+                <Input
+                  isRequired
+                  errorMessage="Please enter a valid email"
+                  label="Email"
+                  labelPlacement="outside"
+                  name="email"
+                  placeholder="Enter your email"
+                  type="email"
+                  value={email}
+                  onValueChange={setEmail}
+                  style={{ width: "490px", height: "64px" }}
+                />
+              </div>
+              <div>
+                <Input
+                  isRequired
+                  errorMessage="Please enter your phone number"
+                  label="Phone Number"
+                  labelPlacement="outside"
+                  name="email"
+                  placeholder="Enter your phone number"
+                  type="text"
+                  value={phoneNumber}
+                  onValueChange={setPhoneNumber}
+                  style={{ width: "490px", height: "64px" }}
+                />
+              </div>
+            </div>
+            <div>
+              <Input
+                isRequired
+                errorMessage="Please type your massage"
+                label="Your Massage"
+                labelPlacement="outside"
+                name="massage"
+                placeholder="type your massage"
+                type="text"
+                value={massage}
+                onValueChange={setMassage}
+                className="w-[490px] lg:w-[1050px] lg:h-[100px]"
+              />
+            </div>
+            <div className="flex pt-2">
+              <Button
+                type="submit"
+                variant="bordered"
+                className="bg-black text-white items-center rounded-full"
+              >
+                Send Massage
+              </Button>
+              <button className="bg-black w-[40px] h-[40px] rounded-full">
+                <img className="pl-2" src={arrow} alt="Arrow" />
+              </button>
+            </div>
+            {submitted && (
+              <div className="text-small text-default-500">
+                {/* You submitted: <code>{JSON.stringify(submitted)}</code> */}
+                Thank you for your valuable Massage
+              </div>
+            )}
+          </Form>
+        </div>
+      </div>
+    </CommonWrapper>
+  );
+};
+
+export default Contact;
--- src/pages/SellingMadeSimple.jsx
+++ src/pages/SellingMadeSimple.jsx
@@ -1,9 +1,157 @@
-import React from 'react'
+import React, { useState } from "react";
+import CommonWrapper from "../components/CommonWrapper";
+import { Input } from "@heroui/react";
 
 const SellingMadeSimple = () => {
-  return (
-    <div>SellingMadeSimple</div>
-  )
-}
+  const [designer, setDesigner] = useState("");
+  const [category, setCategory] = useState("");
+  const [product, setProduct] = useState("");
+  const [productDetails, setProductDetails] = useState("");
 
-export default SellingMadeSimple
+  const [submitted, setSubmitted] = useState(null);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const data = Object.fromEntries(new FormData(e.currentTarget));
+
+    setSubmitted(data);
+    console.log(data);
+  };
+
+  return (
+    <CommonWrapper>
+      <div className="py-5 flex flex-col items-center">
+        <p className="text-[48px] font-semibold leading-[63px] text-center">
+          Welcome
+        </p>
+        <p className="text-center">Email: Badhan.Akter29@Gmail.com</p>
+        <p className="text-center">
+          Not you? <strong>Sign In To My Account</strong>
+        </p>
+      </div>
+
+      <div
+        className="flex flex-col w-full max-w-screen-lg h-auto shrink-0 rounded-[24px] border-dotted mt-[40px] pt-[40px] items-center"
+        style={{
+          background:
+            "linear-gradient(98deg, #F1FBFF 0%, #F1EDEB 49.68%, #F8DAB0 95.53%)",
+        }}
+      >
+        <p className="font-medium text-xl text-center">What Are You Selling?</p>
+        <div className="w-full flex flex-col sm:flex-row lg:flex-row gap-[10px] pt-10 px-5">
+          <Input
+            // isRequired
+            errorMessage="Please enter a designer name"
+            label="Designer"
+            labelPlacement="outside"
+            name="designer"
+            placeholder="Select a Designer name"
+            type="text"
+            value={designer}
+            onValueChange={setDesigner}
+            style={{ height: "64px" }}
+            className="w-full sm:w-[490px] lg:w-[490px]"
+          />
+          <Input
+            // isRequired
+            errorMessage="Please enter a category name"
+            label="Category"
+            labelPlacement="outside"
+            name="category"
+            placeholder="Select a Category"
+            type="text"
+            value={category}
+            onValueChange={setCategory}
+            style={{ height: "64px" }}
+            className="w-full sm:w-[490px] lg:w-[490px]"
+          />
+        </div>
+
+        <div className="pt-5 w-full px-5">
+          <Input
+            // isRequired
+            errorMessage="Please enter a product name"
+            label="Product"
+            labelPlacement="outside"
+            name="product"
+            placeholder="Select a Product name"
+            type="text"
+            value={product}
+            onValueChange={setProduct}
+            // style={{ height: "64px" }}
+            className="w-full sm:w-[490px] lg:w-full"
+          />
+        </div>
+
+        <div className="text-center px-[20px] md:px-[100px] sm:px-[20px] pt-[32px]">
+          <p className="font-medium text-xl">
+            Any Details We Should Know About
+          </p>
+          <p className="font-normal text-xl mt-4">For Example:</p>
+          <ul className="list-disc">
+            <li>
+              <p>Condition: light corner wear, odor, interior pen marks</p>
+            </li>
+            <li>
+              <p>Items included: shoulder strap and lock & key</p>
+            </li>
+            <li>
+              <p>Other details: measurements & item age</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="pt-5 w-full px-5">
+          <Input
+            // isRequired
+            errorMessage="Please enter product details and condition"
+            label="Item Details and Condition"
+            labelPlacement="outside"
+            name="productDetails"
+            placeholder="Type your message......."
+            type="text"
+            value={productDetails}
+            onValueChange={setProductDetails}
+            style={{ height: "190px" }}
+            className="w-full sm:w-[490px] lg:w-full"
+          />
+        </div>
+        <div>
+          <p className="pr-[680px] pt-5">Upload yours photos of Item</p>
+          <div class="grid grid-cols-3 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+            <div class="rounded-[24px] bg-white w-[186px] p-[24px] h-[186px] border-4 border-[1px] border-white  gap-[10px]">
+              himel[1px]
+            </div>
+            <div class="rounded-[24px] bg-white w-[186px] p-[24px] h-[186px] border-4 border-[1px] border-white gap-[10px]">
+              himel
+            </div>
+            <div class="rounded-[24px] bg-white w-[186px] p-[24px] h-[186px] border-4 border-[1px] border-white gap-[10px]">
+              himel
+            </div>
+            <div class="rounded-[24px] bg-white w-[186px] p-[24px] h-[186px] border-4 border-[1px] border-white gap-[10px]">
+              himel
+            </div>
+            <div class="rounded-[24px] bg-white w-[186px] p-[24px] h-[186px] border-4 border-[1px] border-white gap-[10px]">
+              himel
+            </div>
+            <div class="rounded-[24px] bg-white w-[186px] p-[24px] h-[186px] border-4 border-[1px] border-white gap-[10px]">
+              himel
+            </div>
+            <div class="rounded-[24px] bg-white w-[186px] p-[24px] h-[186px] border-4 border-[1px] border-white gap-[10px]">
+              himel
+            </div>
+            <div class="rounded-[24px] bg-white w-[186px] p-[24px] h-[186px] border-4 border-[1px] border-white gap-[10px]">
+              himel
+            </div>
+            <div class="rounded-[24px] bg-white w-[186px] p-[24px] h-[186px] border-4 border-[1px] border-white gap-[10px]">
+              himel
+            </div>
+          </div>
+        </div>
+      </div>
+    </CommonWrapper>
+  );
+};
+
+export default SellingMadeSimple;
